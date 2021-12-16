457bb39ff78fd3583c04adafffa60c3b20629b8b
TheAkseon <81559799+TheAkseon@users.noreply.github.com>
2021-12-17 01:42:51 +0300

    init

---
+++ Score.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Score : MonoBehaviour
+{
+    private int _score = 0;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.TryGetComponent(out Food food))
+        {
+            _score += 50;
+            Debug.Log(_score);
+            Destroy(other.gameObject);
+        }
+        Destroy(other.gameObject);
+    }
+}
